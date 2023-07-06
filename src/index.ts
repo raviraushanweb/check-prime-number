19ec67bba7420e2e3ce1e5981591757156e5fdbd
Ravi Raushan <raviraushanweb@gmail.com>
2023-07-07 02:40:35 +0530

    isPrime function

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export function isPrime(num: number): boolean {
+	if (num <= 1) {
+		return false;
+	}
+	for (let i = 2; i < num; i++) {
+		if (num % i === 0) {
+			return false;
+		}
+	}
+	return true;
+}
